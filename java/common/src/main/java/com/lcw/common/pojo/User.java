5ba4135024ebe26864341e01a634004bd608f18c
yanglaiwei <yanglw@szkexin.com.cn>
2019-05-30 12:35:39 +0800

    no message

---
+++ java/common/src/main/java/com/lcw/common/pojo/User.java
@@ -0,0 +1,73 @@
+package com.lcw.common.pojo;
+
+public class User {
+    private Integer userId;
+
+    private String userName;
+
+    private String userPhone;
+
+    private String userEmail;
+
+    private String verifyCode;
+
+    private String userPwd;
+
+    private Integer userStatus;
+
+    public Integer getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(Integer userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getVerifyCode() {
+        return verifyCode;
+    }
+
+    public void setVerifyCode(String verifyCode) {
+        this.verifyCode = verifyCode;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public void setUserPwd(String userPwd) {
+        this.userPwd = userPwd;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+}
